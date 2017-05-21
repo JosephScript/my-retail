4e08074c1b3bc76bc08bae253077321ed39e97a3
Joseph Szczesniak <nukapunk@gmail.com>
2017-05-21 11:45:41 -0500

    Added quantity

--- src/components/purchase.jsx
+++ src/components/purchase.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import styles from './purchase.css'
 
 export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 1
+    }
+    this.increment = () => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
+    this.decrement = () => {
+      if (this.state.count) {
+        this.setState({
+          count: this.state.count - 1
+        })
+      }
+    }
+  }
+
   render () {
     return (
       <div>
         <div className={styles.quantity}>
           <span>quantity:</span>
-          <button className={styles.minus}>-</button>
-          <input type='number' min='0' max='200' />
-          <button className={styles.plus}>+</button>
+          <div>
+            <a className={styles.buttons} onClick={this.decrement}>-</a>
+            {this.state.count}
+            <a className={styles.buttons} onClick={this.increment}>+</a>
+          </div>
         </div>
       </div>
     )
