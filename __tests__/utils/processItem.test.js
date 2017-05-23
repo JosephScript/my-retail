426dede19a7219e0f4c037ead839c6788933007c
Joseph Szczesniak <nukapunk@gmail.com>
2017-05-23 10:01:59 -0500

    Added test for blank objects

--- __tests__/utils/processItem.test.js
+++ __tests__/utils/processItem.test.js
@@ -1,6 +1,30 @@
 import processItem from '../../src/utils/processItem.js'
 
-describe('Ratings', () => {
+describe('Process Item', () => {
+  it('it formats blank objects correctly', () => {
+    let item = {
+      title: '',
+      price: [],
+      promos: [],
+      highlights: [],
+      images: [],
+      purchasingChannelCode: 0,
+      rating: 0,
+      totalReviews: 0,
+      proRating: 0,
+      conRating: 0,
+      proReview: '',
+      conReview: '',
+      proReviewTitle: '',
+      conReviewTitle: '',
+      proReviewDate: '',
+      conReviewDate: '',
+      proReviewName: '',
+      conReviewName: ''
+    }
+
+    expect(processItem({})).toEqual(item)
+  })
   it('it formats titles correctly', () => {
     let item = {
       CatalogEntryView: [{
